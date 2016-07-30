19cb05395a5bfeb6e8eba4d42b153957efb26fa6
Federico López Cañás <fedelc.us@gmail.com>
2016-07-30 10:23:31 -0300

    Initial commit

---
+++ LaDrone/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LaDrone
+//
+//  Created by Federico Lopez Cañas on 30/7/16.
+//  Copyright © 2016 DroneAlan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
